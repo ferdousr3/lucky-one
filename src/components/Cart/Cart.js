d09c15b5426efff10caa27a772c7d42c8240dad5
ferdousr3 <ferdousr1992@gmail.com>
2022-03-26 20:17:07 +0600

    allmost done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 import { TrashIcon } from "@heroicons/react/solid";
-import './Cart.css'
+import "./Cart.css";
 
-const Cart = (props) => {
-  const {cart, handleRemoveToCart} = props
-
-  console.log(props)
+const Cart = ( props) => {
+  const {
+    id,
+    cart,
+    handleRemoveToCart,
+    handleRandomItem,
+    randomData,
+    handleRemoveToRandom,
+  } = props;
   return (
-    <div className="card border">
+    <div className="card border my-card-image">
       <Card.Body>
+        {/* random items */}
+        <div className="random-item">
+          <img src={randomData.img} alt={randomData.name} />
+          <p className="cart-product-name">{randomData.name}</p>
+        </div>
+        {/* normal selected items */}
         <Card.Title>Add Your Burger</Card.Title>
-        <p>cart:{cart.length}</p>
         <div>
           {cart.map((cartData) => (
             <div className="cart" key={cartData.id}>
@@ -23,10 +33,12 @@ const Cart = (props) => {
             </div>
           ))}
         </div>
-        <Button variant="primary">Choose 1 for Me</Button>
+        <Button onClick={() => handleRandomItem()} variant="primary">
+          Choose 1 for Me
+        </Button>
 
         <Button
-          onClick={() => handleRemoveToCart()}
+          onClick={() => handleRemoveToCart(id)}
           className="mt-2"
           variant="success"
         >
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,7 +8,8 @@ const Shop = () => {
   const [products, setProducts] = useState([]);
   // set product to cart
   const [cart, setCart] = useState([]);
-  // remove product from cart
+  // random item
+   const [random, setRandom] = useState({});
 
   useEffect(() => {
     fetch("product.json")
@@ -37,6 +38,12 @@ const Shop = () => {
     }
   };
 
+  // get random item
+  const handleRandomItem = () => {
+    const newCart = [...cart, random];
+   const randomItem = newCart[Math.floor(Math.random() * newCart.length)];
+    setRandom(randomItem);
+  };
   // Remove all added product from cart
   const handleRemoveToCart = (product) => {
     setCart([]);
@@ -49,6 +56,7 @@ const Shop = () => {
           {/* product part */}
           <div className="col-12 col-sm-12 col-md-9">
             <div className="row">
+              <div className="random-select"></div>
               {products.map((product) => (
                 <div
                   key={product.id}
@@ -64,7 +72,24 @@ const Shop = () => {
           </div>
           {/* cart part */}
           <div className="col-12 col-sm-12 col-md-3">
-            <Cart cart={cart} handleRemoveToCart={handleRemoveToCart}></Cart>
+            <Cart
+              cart={cart}
+              handleRemoveToCart={handleRemoveToCart}
+              handleRandomItem={handleRandomItem}
+              randomData={random}
+            ></Cart>
+          </div>
+        </div>
+        <div className="row py-5">
+          <div className="col-12 col-sm-12 col-md-8 mx-auto">
+            <h4>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+              molestiae quam aspernatur tempora at laudantium cum,
+              necessitatibus odit tempore sapiente! Quaerat ut cumque neque enim
+              accusantium eaque doloremque reprehenderit, libero illum ex non
+              vel, ipsum rerum distinctio quo earum optio ullam alias molestiae
+              iure error consequatur beatae vitae consequuntur. Obcaecati.
+            </h4>
           </div>
         </div>
       </div>
